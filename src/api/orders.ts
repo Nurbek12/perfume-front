37e0dba63614c2ffabda4d245722039e3ba6a312
Nurbek12 <rustamovnurbek02@gmail.com>
2023-12-10 11:53:33 +0500

    9 commit

--- src/api/orders.ts
+++ src/api/orders.ts
@@ -1,6 +1,6 @@
 import api from "."
 import { IOrder } from "../interfaces"
 
-export const getMyOrders = async (id: string) => api.get(`/api/v1/orders/${id}/`)
+export const getMyOrders = async (id: string) => api.get(`/api/v1/orders/?expand=&items=product`)
 
 export const createOrder = async (orderData: IOrder) => api.post('/api/v1/orders/', orderData)
--- src/components/app-profile-orders.vue
+++ src/components/app-profile-orders.vue
@@ -20,9 +20,9 @@
                             {{ item.date_delevery }}
                         </td>
                     </template>
-                    <template #item.count="{ item, column }">
+                    <template #item.count="{ item, index, column }">
                         <td :data-label="column.title">
-                            <v-btn color="primary" density="comfortable" flat @click="dialog=!dialog">
+                            <v-btn color="primary" density="comfortable" flat @click="dialog=true,currentProducts=(index as any)">
                                 <!-- <div class="d-flex align-center gap-1"> -->
                                     {{ item.count }}
                                     <v-icon style="margin-top: 2px;" class="ml-1">mdi-view-list</v-icon>
@@ -32,7 +32,7 @@
                     </template>
                     <template #item.time="{ item, column }">
                         <td :data-label="column.title">
-                            {{ item.time }}
+                            {{ new Date(item.created_at).toLocaleString() }}
                         </td>
                     </template>
                     <!-- <template #item.address="{ item, column }">
@@ -42,14 +42,27 @@
                     </template> -->
                     <template #item.status="{ item, column }">
                         <td :data-label="column.title">
-                            <v-chip label :color="item.status[0]==='в'?'green':'red'" variant="flat" density="comfortable">
-                                {{ item.status }}
+                            <v-chip label :color="{'process':'blue', 'delivered':'green', 'canceled': 'red'}[item.status as 'canceled']" variant="flat" density="comfortable">
+                                {{ {'process':'П процессе', 'delivered':'Доставленный', 'canceled': 'Отменено'}[item.status as 'canceled'] }}
                             </v-chip>
                         </td>
                     </template>
                     <template #item.total="{ item, column }">
                         <td :data-label="column.title">
-                            {{ item.total }} {{ t('sum') }}
+                            {{ item.total_sum }} {{ t('sum') }}
+                        </td>
+                    </template>
+                    <template #item.delivery_type="{ item, column }">
+                        <td :data-label="column.title">
+                            {{ {
+                                "urgent": "Срочная - 20000 сум (в течение дня)",
+                                "not_urgent": "Не срочная  - 0 сум (в течение 3-х дней)",
+                                "pickup":"Самовывоз - 0 сум"}[item.delivery_type as "pickup"] }}
+                        </td>
+                    </template>
+                    <template #item.address="{ item, column }">
+                        <td :data-label="column.title">
+                            {{ item.address }}
                         </td>
                     </template>
                     <template #item.cancel="{ item, column }">
@@ -100,14 +113,30 @@ import { useI18n } from 'vue-i18n'
 import { getMyOrders } from '../api/orders'
 
 const dialog = ref(false)
+const currentProducts = ref(null)
 const { getters } = useStore()
 const { t } = useI18n()
+    //   "created_at": "2023-12-10T06:20:29.879496",
+    //   "updated_at": "2023-12-10T06:20:29.879519",
+    //   "total_sum": 0,
+    //   "address": "54545454",
+    //   "status": "process",
+    //   "delivery_type": "urgent",
+    //   "date_delivery": "с 09:00 до 12:00",
+    //   "items": [
+    //     {
+    //       "order": 4,
+    //       "product": 1,
+    //       "quantity": 1
+    //     }
+    //   ]
 const headers = [
-    { title: 'Способ доставки', key: 'type_delevery', sortable: false },
-    // { title: 'Время доставки', key: 'date_delevery', sortable: false },
+    { title: 'Способ доставки', key: 'delivery_type', sortable: false },
+    // { title: 'Время доставки', key: 'date_delivery', sortable: false },
     { title: 'Продукты', key: 'count', sortable: false },
     { title: 'Время заказа', key: 'time', sortable: false },
     { title: 'Статус', key: 'status', sortable: false },
+    { title: 'Адрес', key: 'address', sortable: false },
     { title: 'Обшая сумма', key: 'total', sortable: false },
     { title: 'Отказать', key: 'cancel', sortable: false },
 ]
@@ -122,16 +151,15 @@ const products = [
     { title: 'Gucci Guilty Pour Femme', count: 1, color: null },
     { title: 'Calvin Klein Euphoria', count: 2, color: {"name": "Blue", "hex": "#0000FF"} },
 ]
-const items = ref([
-    { id: 1, type_delevery: 'Самовывоз', date_delevery: 'с 09:00 до 12:00', count: 4, time: '12.12.2023 12:12', status: 'в процессе', total: 3225000 },
-    { id: 2, type_delevery: 'Не срочная', date_delevery: 'с 09:00 до 12:00', count: 1, time: '10.12.2023 12:12', status: 'отказано', total: 256800 },
-])
+const items = ref<any[]>([])
 
 const init = async () => {
     const { data } = await getMyOrders(getters.user.id)
 
     console.log(data);
+    items.value = data.results
 }
 
 // if(!!getters.user?.id) init()
+init()
 </script>
